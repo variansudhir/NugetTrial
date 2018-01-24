9389c00b43eab114f78c920e6f0b49c8c86cbd8f
variansudhir <sudhir.kr84@gmail.com>
2018-01-24 18:57:13 +0530

    Source Code

---
+++ NugetTrial/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudhir.NugetTrial
+{
+    public class Author
+    {
+        public string GetName()
+        {
+            return "Sudhir Srivastava";
+        }
+    }
+}
